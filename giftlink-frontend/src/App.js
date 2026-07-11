88f98409051e06cc2e78837c2a032d2e9361e9d0
paranjijanardhanvishal <vishaalpj@gmail.com>
2026-07-11 10:42:09 -0400

    Implemented Details Page

--- giftlink-frontend/src/App.js
+++ giftlink-frontend/src/App.js
@@ -1,24 +1,29 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+
 import MainPage from './components/MainPage/MainPage';
+import DetailsPage from './components/DetailsPage/DetailsPage';
 import LoginPage from './components/LoginPage/LoginPage';
 import RegisterPage from './components/RegisterPage/RegisterPage';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
 import Navbar from './components/Navbar/Navbar';
 
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 function App() {
-  return (
-    <>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<MainPage />} />
-        <Route path="/app" element={<MainPage />} />
-        <Route path="/app/login" element={<LoginPage />} />
-        <Route path="/app/register" element={<RegisterPage />} />
-      </Routes>
-    </>
-  );
+    return (
+        <>
+            <Navbar />
+
+            <Routes>
+                <Route path="/" element={<MainPage />} />
+                <Route path="/app" element={<MainPage />} />
+                <Route path="/app/product/:productId" element={<DetailsPage />} />
+                <Route path="/app/login" element={<LoginPage />} />
+                <Route path="/app/register" element={<RegisterPage />} />
+            </Routes>
+        </>
+    );
 }
 
 export default App;
